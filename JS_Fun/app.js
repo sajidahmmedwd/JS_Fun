1b5823fa28db1e0fcfabdf41c84bf2f09f50e627
Sajid Ahmmed <81010996+sajidahmmedwd@users.noreply.github.com>
2024-06-10 09:29:52 +0600

    Add files via upload

---
+++ JS_Fun/app.js
@@ -0,0 +1,9 @@
+let heading = document.getElementById("heading");
+
+const yes = () => {
+    heading.innerText = "Ok! You should practice more and more.."
+}
+
+const ofCourseYes = () => {
+    heading.innerText = "Congratulations!. Your dream comes true..😊"
+}
